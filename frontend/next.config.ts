00a3e30e11e7ab2ec786f3bf1e4b508f644ccddc
Nelo <nellorchamp@gmail.com>
2026-05-01 00:16:21 +0800

    Allow frontend build with Sentry config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,23 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "swifthire-xr",
+  project: "swifthire-frontend",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+
+  webpack: {
+    automaticVercelMonitors: true,
+    treeshake: {
+      removeDebugLogging: true,
+    },
+  },
+});
